10802f151a69dbaa71d588680efbed6d430a66bd
emmyachieng <emmyachieng97@gmail.com>
2021-04-26 06:34:22 +0300

    Assessment's tasks -Fetch blog -Change string input to uppercase output

---
+++ lib/blogger.ex
@@ -0,0 +1,29 @@
+defmodule Blogger do
+  @moduledoc """
+  Documentation for `Blogger`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Blogger.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def fetch_blog do
+    HTTPoison.get("https://medium.com/podiihq/quick-connect-to-your-amazon-ec2-linux-instance-through-the-command-line-6c682960ef91")
+  end
+
+  def upcase(string) when is_binary(string) do
+    string
+    |> String.upcase()
+    |> String.split(string, [" ", "(", ")"])
+    |> IO.puts 
+  end
+end
